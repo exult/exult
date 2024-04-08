fa0d02c5b041a38c137a78a448643d51bb9af572
NotStanley4330 <you@example.com>
2024-04-08 11:37:34 -0600

    Added sfx volume functionality
    Removed changes
    
    Extended shape_info.txt to allow a frame to halt sfx.
    
    Fixed the frame value so energy field correctly halts!
    
    Added description of halt_frame extension in shape_info.txt
    
    Removed extraneous print statements
    
    clang-formatted
    
    Gave energy field sfx a chance to play to better mimic original behavior
    
    Reworked sfx to allow individual volume levels
    
    tweaked individual levels
    
    Deleted Is_halt_frame function
    
    Fixed dexriptions
    
    Fixed indentation

--- objs/animate.cc
+++ objs/animate.cc
@@ -175,11 +175,10 @@ void Shape_sfx::stop() {
 }
 
 inline void Shape_sfx::set_looping() {
-	looping = sfxinf
-					  ? (sfxinf->get_sfx_range() == 1
-						 && sfxinf->get_chance() == 100
-						 && !sfxinf->play_horly_ticks())
-					  : false;
+	looping = sfxinf ? (sfxinf->get_sfx_range() == 1
+						&& sfxinf->get_chance() == 100
+						&& !sfxinf->play_horly_ticks())
+					 : false;
 }
 
 /*
@@ -238,8 +237,11 @@ void Shape_sfx::update(bool play) {
 	}
 
 	dir               = 0;
-	const int  volume = AUDIO_MAX_VOLUME;    // Set volume based on distance.
+	//set volume based on Distance and SFX volume
+	const int  volume = static_cast<int>(AUDIO_MAX_VOLUME * (sfxinf->get_volume() / 100.0));
 	const bool halt   = Get_sfx_out_of_range(gwin, obj->get_center_tile());
+	std::cout << "Volume of sfx for shape num" << obj->get_shapenum() << " is ";
+	std::cout << volume << std::endl;
 
 	if (play && halt) {
 		play = false;
