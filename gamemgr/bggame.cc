exult/exult

commit 613bf6c60def4566117a5d1fb0b3c15c88692715

Correctly unsetting 	SDL_HINT_RETURN_KEY_HIDES_IME.

gamemgr/bggame.cc
@@ -2038,11 +2038,12 @@ bool BG_Game::new_game(Vga_file& shapes) {
 														// visible initially
 	}
 	SDL_Window* window = gwin->get_win()->get_screen_window();
-#if !defined(SDL_PLATFORM_IOS) && !defined(ANDROID)
-	if (!SDL_TextInputActive(window)) {
-		TouchUI::startTextInput(window);
+
+	if (touchui != nullptr) {
+		if (!SDL_TextInputActive(window)) {
+			TouchUI::startTextInput(window);
+		}
 	}
-#endif
 	do {
 		Delay();
 		if (redraw) {
@@ -2122,7 +2123,6 @@ bool BG_Game::new_game(Vga_file& shapes) {
 							TouchUI::startTextInput(window);
 						} else {
 							SDL_StopTextInput(window);
-							SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "0");
 						}
 					}
 					redraw = true;
@@ -2250,6 +2250,7 @@ bool BG_Game::new_game(Vga_file& shapes) {
 	if (SDL_TextInputActive(window)) {
 		SDL_StopTextInput(window);
 	}
+	SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "0");
 	// Hide mouse on way out to clear the mouse's dirty box
 	if (Mouse::mouse()) {
 		Mouse::mouse()->hide();

gamemgr/sigame.cc
@@ -1416,11 +1416,12 @@ bool SI_Game::new_game(Vga_file& shapes) {
 	Avatar_default_skin* defskin  = Shapeinfo_lookup::GetDefaultAvSkin();
 	Skin_data*           skindata = Shapeinfo_lookup::GetSkinInfoSafe(defskin->default_skin, defskin->default_female, true);
 	SDL_Window*          window   = gwin->get_win()->get_screen_window();
-#if !defined(SDL_PLATFORM_IOS) && !defined(ANDROID)
-	if (!SDL_TextInputActive(window)) {
-		TouchUI::startTextInput(window);
+	if (touchui != nullptr) {
+		if (!SDL_TextInputActive(window)) {
+			TouchUI::startTextInput(window);
+			SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "1");
+		}
 	}
-#endif
 	do {
 		Delay();
 		if (redraw) {
@@ -1488,8 +1489,10 @@ bool SI_Game::new_game(Vga_file& shapes) {
 						selected = 0;
 					} else if (selected == 0 && touchui != nullptr) {
 						if (!SDL_TextInputActive(window)) {
+							SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "1");
 							TouchUI::startTextInput(window);
 						} else {
+							SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "0");
 							SDL_StopTextInput(window);
 						}
 					}
@@ -1618,6 +1621,7 @@ bool SI_Game::new_game(Vga_file& shapes) {
 	if (SDL_TextInputActive(window)) {
 		SDL_StopTextInput(window);
 	}
+	SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "0");
 	// Hide mouse on way out to clear the mouse's dirty box
 	if (Mouse::mouse()) {
 		Mouse::mouse()->hide();

gumps/Newfile_gump.cc
@@ -247,6 +247,7 @@ Newfile_gump::~Newfile_gump() {
 		if (!gumpman->gump_mode() || (!gumpman->modal_gump_mode() && gumpman->gumps_dont_pause_game())) {
 			touchui->showGameControls();
 		}
+		SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "0");
 	}
 }
 
@@ -758,7 +759,6 @@ bool Newfile_gump::mouse_up(
 			SetTextInputArea(window);
 			TouchUI::startTextInput(window);
 		} else {
-			SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "0");
 			SDL_StopTextInput(window);
 		}
 		result = true;

commit 997b4d3d2eb58af31c038ca407addaf7fcdbf5ea

Fixing textinput for entering your name that I broke in 613bf6c60

gamemgr/bggame.cc
@@ -2039,10 +2039,12 @@ bool BG_Game::new_game(Vga_file& shapes) {
 	}
 	SDL_Window* window = gwin->get_win()->get_screen_window();
 
-	if (touchui != nullptr) {
-		if (!SDL_TextInputActive(window)) {
+	if (!SDL_TextInputActive(window)) {
+		if (touchui != nullptr) {
 			SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "1");
 			TouchUI::startTextInput(window);
+		} else {
+			SDL_StartTextInput(window);
 		}
 	}
 	do {

gamemgr/sigame.cc
@@ -1416,10 +1416,12 @@ bool SI_Game::new_game(Vga_file& shapes) {
 	Avatar_default_skin* defskin  = Shapeinfo_lookup::GetDefaultAvSkin();
 	Skin_data*           skindata = Shapeinfo_lookup::GetSkinInfoSafe(defskin->default_skin, defskin->default_female, true);
 	SDL_Window*          window   = gwin->get_win()->get_screen_window();
-	if (touchui != nullptr) {
-		if (!SDL_TextInputActive(window)) {
+	if (!SDL_TextInputActive(window)) {
+		if (touchui != nullptr) {
 			SDL_SetHint(SDL_HINT_RETURN_KEY_HIDES_IME, "1");
 			TouchUI::startTextInput(window);
+		} else {
+			SDL_StartTextInput(window);
 		}
 	}
 	do {